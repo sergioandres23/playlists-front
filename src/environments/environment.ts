b6d09ae57c13e13097757602bf93a6556a08e6b4
Sergio Gomez <sergio.gomezv@udea.edu.co>
2025-08-20 11:47:41 -0500

    feat(config): Configure API environment and DTO interfaces

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  // Asegúrate de que esta URL coincida con la de tu microservicio Spring Boot
+  // Si tu backend corre en un puerto diferente o en otra máquina, ajústala.
+  apiUrl: 'http://localhost:8080'
+};
