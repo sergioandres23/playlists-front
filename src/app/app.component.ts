373e5defeb8591585df6fbb01e65b3a923e1981e
Sergio Gomez <sergio.gomezv@udea.edu.co>
2025-08-20 11:50:48 -0500

    feat(ui): Create basic playlist management UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import { PlaylistService, PlaylistDTO, SongDTO } from './playlist.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: false
 })
 export class AppComponent implements OnInit {
   title = 'playlists-front';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component'; // ¡Esta línea es crucial!
+import { AppComponent } from './app.component'; 
 
 @NgModule({
   declarations: [
